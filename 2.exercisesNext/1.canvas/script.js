3f24084af95d876a38c9e3e352e31eca81ff9fe2
j-pard <pardons.jonathan@gmail.com>
2020-02-03 16:55:41 +0100

    Rework of Piou-Piou

--- 2.exercisesNext/1.canvas/script.js
+++ 2.exercisesNext/1.canvas/script.js
@@ -1,112 +1,227 @@
 (() => {
+      const start = document.getElementById("start");
+      const timer = document.getElementById("timer");
+      const counter = document.getElementById("count");
+
       const canvas = document.getElementById("canvas");
       const ctx = canvas.getContext("2d");
+      ctx.lineCap = "round";
+
+      let play = false;
+      let playInterval;
+      let touched = 0;
       
       const minX = 0;
       const maxX = 800;
       const minY = 0;
       const maxY = 600;
+      const ballInitY = maxY - 100;
 
-      const baseWidth = 100;
-      const baseHeight = 30;
+      const boatWidth = 150;
+      const boatHeight = 60;
 
-      let base = {
-            width: baseWidth,
-            height: baseHeight,
-            x: (maxX - baseWidth)/2,
-            y: (maxY - (baseHeight + 20)),
-            vx : 0.025,
-            vy: 0,
-            color: "gray",
-            draw: function() {
-                  ctx.fillStyle = this.color,
-                  ctx.fillRect(this.x, this.y, this.width, this.height);
-            },
-            move: function(direction) {
-                  clear("base");
-                  if(direction == 37) { //LEFT
-                        this.x -= this.vx;
-                        this.draw();
-                  }
-                  else if(direction == 39) { //RIGHT
-                        this.x += this.vx;
-                        this.draw()
-                  }
+      let boatX = (maxX-boatWidth)/2;
+      let boatY = (maxY-boatHeight)-10;
+      const vx = 8;
 
-                  //LIMITS
-                  if (this.x < minX) {
-                        this.x = minX;
-                  }
-                  else if (this.x > (maxX - this.width)) {
-                        this.x = (maxX - this.width);
-                  }
-            },
+      let ballExist = false;
+      let ballX = boatX + boatWidth/2;
+      let ballY = ballInitY;
+      let ballInt;
 
-      };
+      const enemySize = 20;
+      const enemyMinX = minX + (boatWidth/2) + 5;
+      const enemyMaxX = maxX - (boatWidth/2) - 5;
+      const enemyMinY = minY + 2*enemySize;
+      const enemyMaxY = (maxY/3) - enemySize;
+      let enemyExist = false;
+      let enemyPosition = [];
 
-      let ball = {
-            x: base.x + (base.width/2),
-            y: base.y - 30,
-            vx: 0,
-            vy: -15,
-            radius: 10,
-            color: "red",
-            exist: false,
-            draw: function() {
-                  ctx.beginPath();
-                  ctx.arc(staticX, this.y, this.radius, 0, Math.PI*2, true);
-                  ctx.closePath();
-                  ctx.fillStyle = this.color;
-                  ctx.fill();
-            },
-            update: function() {
-                  ctx.beginPath();
-                  ctx.arc(staticX, this.y += this.vy, this.radius, 0, Math.PI*2, true);
-                  ctx.closePath();
-                  ctx.fillStyle = this.color;
-                  ctx.fill();
+      const drawBoat = (x, y) => {
 
-                  if (ball.x > maxX || ball.y > maxY || ball.x < minX || ball.y < minY) {
-                       this.x = base.x + baseWidth/2;
-                       this.y = maxY - 50; 
-                       this.exist = false;
-                  }
+            // Shell
+            ctx.beginPath();
+            ctx.moveTo(x,y);
+            ctx.lineTo(x+20,y-20);
+            ctx.lineTo(x+130,y-20);
+            ctx.lineTo(x+150,y);
+            ctx.lineTo(x+130,y+20);
+            ctx.lineTo(x+20,y+20);
+            ctx.lineTo(x,y);
+            ctx.fillStyle = "peru";
+            ctx.strokeStyle = "sienna"; // Line color
+            ctx.lineWidth = 5; 
+            ctx.fill();             // In
+            ctx.stroke();          // Ext
+
+            // Masts
+            ctx.beginPath();
+            ctx.moveTo(x+50,y+30);
+            ctx.lineTo(x+50,y-30);
+            ctx.moveTo(x+100,y+30);
+            ctx.lineTo(x+100,y-30);
+            ctx.lineWidth = 10;
+            ctx.stroke();
+      }
+
+      const ballDraw = (x, y) => {
+            ballX = boatX + boatWidth/2;
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, Math.PI*2, true);
+            ctx.closePath();
+            ctx.fillStyle = "red";
+            ctx.fill();
+            ballExist = true;
+      }
+
+      const ballUpdate = () => {
+            clear();
+            drawBoat(boatX, boatY);
+            ctx.beginPath();
+            ctx.arc(ballX, ballY-=3, 5, 0, Math.PI*2, true);
+            ctx.closePath();
+            ctx.fillStyle = "red";
+            ctx.fill();
+            if (ballY >= maxY || ballY <= minY) {
+                  reArm();
+                  clear();
+                  drawBoat(boatX, boatY);
             }
-      }; 
+            checkCollision();
+      }
 
-      let checkPosition = () => {
-            return base.x + base.width/2;
-      };
+      const drawEnemy = () => {
+            if(!enemyExist) {
+                  enemyPosition[0] = Math.floor(Math.random() * (enemyMaxX - enemyMinX) + enemyMinX);
+                  enemyPosition[1] = Math.floor(Math.random() * (enemyMaxY - enemyMinY) + enemyMinY);
+            }
 
-      const clear = (zone) => {
-            ctx.fillStyle = "white";
-            if(zone == "base") {
-                  ctx.fillRect(base.x - 100, base.y, base.x + base.width + 100, base.y + base.height);
+            let x = enemyPosition[0];
+            let y = enemyPosition[1];
+            ctx.beginPath();
+            ctx.arc(x, y, enemySize, 0, Math.PI*2, true);
+            ctx.closePath();
+            ctx.strokeStyle = "gray";
+            ctx.lineWidth = 5; 
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(x-10, y);
+            ctx.lineTo(x+10, y);
+            ctx.moveTo(x, y-10);
+            ctx.lineTo(x, y+10);
+            ctx.closePath();
+            ctx.strokeStyle = "orange";
+            ctx.lineWidth = 5; 
+            ctx.stroke();
+            enemyExist = true;
+            return enemyPosition = [x, y];            
+      }
+
+      const clear = () => {
+            ctx.fillStyle = "lightblue";
+            ctx.fillRect(minX, minY, maxX, maxY);
+            drawEnemy();
+      }
+
+      const reArm = () => {
+            ballY = ballInitY;
+            ballExist = false;
+            clearInterval(ballInt);
+            
+      }
+
+      const checkCollision = () => {
+            let zoneMaxX = enemyPosition[0]+enemySize;
+            let zoneMinX = enemyPosition[0]-enemySize;
+            let zoneY = enemyPosition[1]+enemySize;
+            if(ballX <= zoneMaxX && ballX >= zoneMinX && ballY <= zoneY) {
+                  console.log("Touché !");
+                  reArm();
+                  enemyExist = false;
+                  clear();
+                  drawBoat(boatX, boatY);
+                  touched++;
+                  setScore();
+            }
+      }
+
+      const setScore = (arg) => {
+            if(arg == 0) {
+                  touched = 0;
+                  counter.textContent = 0;
             }
             else {
-                  ctx.fillRect(minX, minY, maxX, maxY);
+                  counter.textContent = touched;
             }
+      }
 
-      };
-
-      base.draw();
-      setInterval(() => {
+      const init = () => {
+            play = true;
+            let ms = 0;
+            let sec = 0;
+            let min = 0;
+            setInterval(() => {
+                  ms+=10;
+                  if(ms == 100) {
+                        ms = 0;
+                        sec++;
+                  }
+                  if(sec == 60) {
+                        sec = 0;
+                        min++;
+                  }
+                  timer.textContent = min + ":" + sec + ":" + ms;
+            }, 100);
+            setScore(0);
             clear();
-            base.draw();
-            // Interractions
-            document.addEventListener("keydown", (e) => {
-                  if(e.keyCode == 37 || e.keyCode == 39) {
-                        base.move(e.keyCode);
-                  }
-                  if(e.keyCode == 32 && !ball.exist) { //FIRE
-                        staticX = checkPosition();
-                        ball.draw();
-                        ball.exist = true;
-                  }
-            });
-            if (ball.exist) {
-                  ball.update();
-            }
-      }, 20);
+            drawBoat(boatX, boatY);
+            drawEnemy(enemyPosition[0], enemyPosition[1]);
+      }
+      // --------------------------------------------
+      // RUNNING
 
+      start.addEventListener("click", () => {
+            init();
+            start.blur();
+      })
+
+      document.addEventListener("keydown", (e) => {
+            if(play == true) {
+                  if(e.key == "ArrowLeft" || e.key == "ArrowRight") {
+                        clear();
+                        if(e.key == "ArrowLeft") {
+                              boatX -= vx;
+                              drawBoat(boatX, boatY);
+                        }
+                        else if(e.key == "ArrowRight") {
+                              boatX += vx;
+                              drawBoat(boatX, boatY);
+                        }
+                        //Limits
+                        if(boatX <= minX) {
+                              boatX = minX+5;
+                        }
+                        else if(boatX >= maxX-boatWidth) {
+                              boatX = maxX-boatWidth-5;
+                        }
+                  }
+                  if(e.key == " " && !ballExist) {
+                        ballDraw(boatX + (boatWidth/2), ballY);
+                              ballInt = setInterval(() => {
+                                    ballUpdate();
+                              }, 5);   
+                  }
+
+                  if(touched >= 10) {
+                        if(confirm("Victoire ! Voulez-vous rejouer ?")) {
+                              document.location.reload();
+                        }
+                  }
+      
+            }
+            else {
+
+            }
+      });
 })();
